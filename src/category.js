fff195123750bef82b856c402137592d486ed439
jimboweb <jimboweb@gmail.com>
2018-09-22 11:37:10 -0400

    category and categoryList

---
+++ src/category.js
@@ -0,0 +1,11 @@
+import React;
+import './App.css';
+
+
+function category(props){
+    return(
+        <div className="category" id={props.id}>
+
+        </div>
+    );
+}
---
+++ src/categoryList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class categoryList extends Component {
+    render() {
+        return (
+            <div className="categoryList" id-"categoryRoot">
+            </div>
+        );
+    }
+}
+
+export default categoryList;
