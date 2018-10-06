cda65ec0aac7bfadbe14a6f6a88ee8c810abb424
jimboweb <jimboweb@gmail.com>
2018-10-06 19:00:28 -0400

    turned register service worker on again

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
-serviceWorker.unregister();
+registerServiceWorker();
+//serviceWorker.unregister();
