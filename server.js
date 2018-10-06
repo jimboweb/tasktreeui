7ba03aef5a708dc63503b8265fc2a38120e15bce
jimboweb <jimboweb@gmail.com>
2018-10-06 18:17:32 -0400

    changed start to npm start

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
+const dotEnv = require('dotenv');
+dotEnv.config();
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
