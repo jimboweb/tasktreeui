cbd8e734f5334426ca1d8018c55908a350f343e4
jimboweb <jimboweb@gmail.com>
2018-10-14 12:33:13 -0400

    who knows what this is

---
+++ build/precache-manifest.6f7a4946f421f41340a0331eabc9182a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "99a2e86c540525edbaf8",
+    "url": "./static/js/main.99a2e86c.chunk.js"
+  },
+  {
+    "revision": "fee1467b5d4e4df5737b",
+    "url": "./static/js/1.fee1467b.chunk.js"
+  },
+  {
+    "revision": "99a2e86c540525edbaf8",
+    "url": "./static/css/main.089527e0.chunk.css"
+  },
+  {
+    "revision": "d6dec538a3fa26b38c32c908f74b1b61",
+    "url": "./index.html"
+  }
+];
