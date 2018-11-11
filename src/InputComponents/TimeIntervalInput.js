2faadae4ebe702ac29d8298db8907036a183e44c
jimboweb <jimboweb@gmail.com>
2018-11-11 12:05:14 -0500

    Update src/InputComponents/TimeIntervalInput.js

--- src/InputComponents/TimeIntervalInput.js
+++ src/InputComponents/TimeIntervalInput.js
@@ -10,6 +10,7 @@ function TimeIntervalInput(props) {
                 <input
                     type='number'
                     name={props.name.toLowerCase()}
+                    //TODO 181111: change props.value to current task item value in each of these
                     value={props.value}
                     onChange = {props.handlers.handleChange}
                     {...this.props.attributes}
