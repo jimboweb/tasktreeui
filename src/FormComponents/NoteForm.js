06159055f5d267844c068a90b1731620e409e314
jimboweb <jimboweb@gmail.com>
2019-02-07 16:39:48 -0500

    created note form and note object

--- src/FormComponents/NoteForm.js
+++ src/FormComponents/NoteForm.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react';
 import './App.css';
-//TODO 190407: make Note form
+import InputTypes from "../Enums/InputTypes";
+import formUtil from "../util/formUtil";
+import FormBuilder from "./TaskForm";
 class NoteForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            inputFields: [
+                {props: {Note: 'name',}, type: InputTypes.TEXT},
+            ],
+
+
+        };
+    }
+
+    submitAction=()=>{
+        const form = document.getElementById("form"+this.props.id);
+        const formData = formUtil.formInputsToObject(form);
+        const noteJson = Object.assign(this.props.data,formData);
+        this.props.submitAction(noteJson)
+
     }
 
 
     render() {
+        return <div className="noteInput" id={"form"+this.props.id}>
+            <FormBuilder
+                inputFields={this.state.inputFields}
+                handlers = {this.state.handlers}
+                data = {this.props.data}
+            />
+        </div>
     }
 }
+}
 
-export default ComponentName;
+export default NoteForm;
--- src/ObjectClasses/NoteObject.js
+++ src/ObjectClasses/NoteObject.js
@@ -1,1 +1,10 @@
-//TODO 190207: make note object
+class NoteObject{
+    constructor(note, timeStamp, parent, parentType){
+        this.timeStamp = timeStamp?timeStamp:new Date();
+        this.note=note?note:"";
+        this.parent=parent?parent:0;
+        this.parentType=parentType?parentType:"";
+    }
+}
+
+export default NoteObject;
