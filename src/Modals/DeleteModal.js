096ec6c685dcfb813c8848933093a70b34470bca
jimboweb <jimboweb@gmail.com>
2019-02-13 19:06:51 -0500

    just used react-modal
    I want to get this done

--- src/Modals/DeleteModal.js
+++ src/Modals/DeleteModal.js
@@ -1,38 +1,73 @@
 import React from "react";
 import NewParentSearch from "../InputComponents/NewParentSearch";
-import Modal from "./Modal";
-import DeleteModalContent from "./DeleteModalContent";
+import Modal from 'react-modal'
+
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+};
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#userRoot')
+
 
 /**
  * @param props: !visible, !componentType, !componentName !parentTypes !rebaseChildren()  !deleteChildren()
  */
-class DeleteModal extends Modal  {
+class DeleteModal extends React.Component  {
     constructor(props){
         super(props);
         this.state={
-            visible: false,
-            rebaseChildren : (newParent) => {
-                this.props.rebaseChildren(newParent);
-                this.hide();
-            },
-            deleteChildren:()=>{
-                this.props.deleteChildren();
-                this.hide()
-            },
+            modalIsOpen:false
+        }
 
-            children: <fragment>
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+
+    }
+
+
+
+    openModal() {
+        this.setState({modalIsOpen: true});
+    }
+
+    afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        this.subtitle.style.color = '#f00';
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
+
+
+    render(){
+            return <Modal
+                isOpen={this.state.modalIsOpen}
+                onAfterOpen={this.afterOpenModal}
+                onRequestClose={this.closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <button onClick={this.closeModal} id='closeModalButton'>close</button>
                 The {this.props.componentType} {this.props.componentName} that you are deleting contains child Tasks or Events.
                 Do you want to delete the children or assign them to a new parent?
                 <button className="deleteChildren" onClick={this.state.deleteChildren}>Delete Children</button>
                 <button className="cancelDelete">Don't Delete</button>
                 <NewParentSearch parentTypes={this.props.parentTypes} buttonAction = {this.state.rebaseChildren}/>
-            </fragment>,
-        }
+
+            </Modal>
+            }
 
     }
-
-}
-
 
 
 export default DeleteModal;
