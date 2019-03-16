20dc8a2c68b75e26f5df6dffdf3380af9bd2125a
jimboweb <jimboweb@gmail.com>
2019-03-16 11:15:48 -0400

    fixme in delete function
    doesn't show function as deleted.

--- src/ListComponents/TaskList.js
+++ src/ListComponents/TaskList.js
@@ -38,6 +38,8 @@ class TaskList extends Component {
     deleteTaskRebaseChildren=(taskId, newParentType, newParentId)=>{
         this.taskApiCalls.deleteTaskRebaseChildren(taskId, this.props.xAccessToken,newParentType, newParentId, ()=>this.props.update);
     };
+
+    //fixme 190316: delete works but doesn't remove deleted task from page.
     deleteTaskAndChildren=(taskId)=>{
         this.taskApiCalls.deleteTaskAndChildren(taskId,this.props.xAccessToken,()=>this.props.update)
     };
